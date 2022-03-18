dc7c13e7d751a02adfec101cf30bbbed921e3e84
martin <kino3y3@gmail.com>
2022-03-19 00:16:58 +0900

    search form

--- app/api/films.js
+++ app/api/films.js
@@ -1,5 +1,8 @@
-export async function getFilms() {
+export async function getFilms(title) {
   const response = await fetch("https://ghibliapi.herokuapp.com/films");
   const films = await response.json();
-  return films;
+  // return films;
+  return films.filter((film) =>
+  title ? film.title.toLowerCase().includes(title.toLowerCase()) : true
+  );
 }
--- app/routes/films/index.jsx
+++ app/routes/films/index.jsx
@@ -1,30 +1,43 @@
 import React from "react";
-import { useLoaderData } from "remix";
+import { useLoaderData, Form } from "remix";
 import { getFilms } from "~/api/films";
 
 // SERVER SIDE
-export const loader = async () => {
-  //   const response = await fetch("https://ghibliapi.herokuapp.com/films");
-  //   return response.json();
-  return getFilms();
+export const loader = async ({request}) => {
+  const url = new URL(request.url)
+  const title = url.searchParams.get('title')
+  return getFilms(title);
 };
 
 // CLIENT SIDE
 export default function FilmIndex() {
   const films = useLoaderData();
-  return (
-    <div>
-      Films
+  return (<div className="p-16 font-sans">
+      <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
+      <Form action="" className="py-5">
+        <label htmlFor="" className="font-bold">
+          Search{` `}
+          <input
+            type="text"
+            name="title"
+            placeholder="Type a title..."
+            className="border-2 rounded py-2 px-3"
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+        >
+          Search
+        </button>
+      </Form>
       <div className="grid grid-cols-4 gap-4">
-        {films.map((film) => (
-          <div className="cursor-pointer hover:scale-105 hover:font-bold hover:shadow-2xl">
+        {films.map((film) => (<div className="cursor-pointer hover:scale-105 hover:font-bold hover:shadow-2xl">
             <div>{film.title}</div>
             <img src={film.image} alt={film.title} />
-          </div>
-        ))}
+          </div>))}
       </div>
-    </div>
-  );
+    </div>);
 }
 
 export const meta = () => {
